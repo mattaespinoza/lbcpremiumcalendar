9610f8461b3487a78aeb6af8bc71096962b2a4dd
Matthew Espinoza <matthewaespinoza@gmail.com>
2023-05-07 22:02:48 -0500

    n

--- src/pages/api/rss.ts
+++ src/pages/api/rss.ts
@@ -17,6 +17,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { data: events, error } = await supabase
     .from('events')
     .select('*')
+    .eq('city_calendar','San Antonio')
     .order('start_time', { ascending: true });
 
   if (error) {
